4b0402a5e6394eb2dd5c95293d743be5d7002d62
viniciuscmac <viniciuscmac@gmail.com>
2017-03-17 14:36:19 -0300

    Adding Survivor Functionality

--- js/formController.js
+++ js/formController.js
@@ -1,13 +1,31 @@
 (function() {
-  angular.module('zombieSocial').controller('FormController', FormController);
+  angular.module('zombieSocial').controller('FormController',FormController);
 
-  FormController.$inject = ['Survivor'];
+  FormController.$inject = ['Survivor','$window'];
 
-  function FormController(FormController) {
+  function FormController(Survivor, $window) {
   	var vm = this;
 
-    vm.title = "zombieSocial API"
+    vm.newSurvivor = new Survivor();
+    vm.add = add;
 
+    function add(){
+      vm.newSurvivor.lonlat = "POINT("+vm.lat+","+vm.lng+")";
+      vm.newSurvivor.items = "Water:" + vm.water + ";Food:" + vm.food + ";Medication:" + vm.medication + ";Ammunition:" + vm.ammunition;
+      vm.newSurvivor.$save(function(result){
+        vm.survivorData = result;
+        console.log(result);
+        $window.alert("Your id is: "+result.id+"");
+        vm.water="";
+        vm.food="";
+        vm.medication="";
+        vm.ammunition="";
+        vm.newSurvivor = {};
+        vm.lat= "";
+        vm.lng="";
+      })
+
+    }
 
   }
 })();
